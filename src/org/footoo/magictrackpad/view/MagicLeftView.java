2f5b8d243fdd061bdb4ebee55af4fd841e8818e4
countcain <countcainht@gmail.com>
2013-04-28 17:28:16 +0800

    better view

--- src/org/footoo/magictrackpad/view/MagicLeftView.java
+++ src/org/footoo/magictrackpad/view/MagicLeftView.java
@@ -9,52 +9,98 @@ import android.view.View;
 
 public class MagicLeftView extends View {
 
-	private int px, py;
-	private Paint gPaint;	//画手势路径的paint
+	private int px, py;// 圆心
+	private Paint gPaint; // 画手势路径的paint
 	private Paint outerPaint;
 	private Paint innerPaint;
-	
+	private boolean currentRotation;
+	private double currentX;
+	private double currentY;
+	private Paint currentPaint;
+
 	public MagicLeftView(Context context) {
 		super(context);
 		setPaint();
+		currentRotation = false;
 	}
-	
-	public MagicLeftView(Context context, AttributeSet attr){
+
+	public MagicLeftView(Context context, AttributeSet attr) {
 		super(context, attr);
 		setPaint();
+		currentRotation = false;
 	}
-	public MagicLeftView(Context context, AttributeSet attr, int arg){
+
+	public MagicLeftView(Context context, AttributeSet attr, int arg) {
 		super(context, attr, arg);
 		setPaint();
+		currentRotation = false;
 	}
-	
-	private void setPaint(){
+
+	private void setPaint() {
 		gPaint = new Paint();
 		gPaint.setColor(0x44000000);
 		gPaint.setStrokeWidth(2);
-		
+
 		outerPaint = new Paint();
 		outerPaint.setColor(0xFFFFA500);
 		outerPaint.setStrokeWidth(2);
 		innerPaint = new Paint();
 		innerPaint.setColor(0x00000000);
 		innerPaint.setStrokeWidth(2);
+		currentPaint = new Paint();
+		currentPaint.setColor(0x00000000);
 	}
 
 	@Override
-	protected void onDraw(Canvas canvas){
+	protected void onDraw(Canvas canvas) {
 		super.onDraw(canvas);
-		canvas.drawCircle(px, py, px,  outerPaint);
+		canvas.drawCircle(px, py, px, outerPaint);
 		canvas.drawCircle(px, py, px, innerPaint);
 		innerPaint.setColor(0xFF8B4726);
-		canvas.drawCircle(px, py, 2*px/3, innerPaint);
+		canvas.drawCircle(px, py, 2 * px / 3, innerPaint);
+
+		if (currentRotation) {
+			canvas.drawCircle((float)currentX, (float)currentY, px / 6, currentPaint);
+		}
 	}
-	
+
 	@Override
-	public boolean onTouchEvent(MotionEvent event){
+	public boolean onTouchEvent(MotionEvent event) {
 		float x = event.getX();
 		float y = event.getY();
+
+		switch (event.getAction()) {
+		case MotionEvent.ACTION_DOWN:
+			if (isRotation(x, y)) {
+				currentRotation = true;
+				circle_start(x, y);
+				invalidate();
+			}
+			break;
+		case MotionEvent.ACTION_MOVE:
+			break;
+		case MotionEvent.ACTION_UP:
+			break;
+		}
+
 		return true;
+	}
+
+	//此函数求出当前圆心的位置。
+	private void circle_start(double x, double y) {
+		double alpha = Math.cosh((px-x)/(py-y));
+		double radius = 5*px/6;
+		currentX = px - Math.cos(alpha)*radius;
+		currentY = py - Math.sin(alpha)*radius;
+	}
+
+	private boolean isRotation(float x, float y) {
+		if ((Math.pow(x - px, 2) + Math.pow(y - py, 2)) <= Math.pow(2 * px / 3,
+				2)) {
+			return false;
+		} else {
+			return true;
+		}
 	}
 
 	/**
